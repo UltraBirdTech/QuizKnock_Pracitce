9eaf5a6e8afdd94ec67ec59c26001f2011c4175f
UltraBirdTech <ultrabirdrepti@gmail.com>
2021-11-21 12:13:34 +0900

    generate alphabet list

--- rsa.py
+++ rsa.py
@@ -4,6 +4,7 @@
 # RSA 暗号をプログラムで解いてみる。
 ##################################
 import re
+import string
 
 def main():
     p = 37
@@ -47,37 +48,10 @@ def calc_M(c, d, n):
     return (c**d) % n
 
 def exchange_M_to_str(m):
-    alphabet_dict = {
-        1: "A",
-        2: "B",
-        3: "C",
-        4: "D",
-        5: "E",
-        6: "F",
-        7: "G",
-        8: "H",
-        9: "I",
-        10: "J",
-        11: "K",
-        12: "L",
-        13: "M",
-        14: "N",
-        15: "O",
-        16: "P",
-        17: "Q",
-        18: "R",
-        19: "S",
-        20: "T",
-        21: "U",
-        22: "V",
-        23: "W",
-        24: "X",
-        25: "Y",
-        26: "Z",
-    }
+    alphabet_list = list(string.ascii_uppercase)
     result_str = ''
     for s in  list(filter(None, re.split('(..)', str(m)))):
-        result_str += alphabet_dict.get(int(s))
+        result_str += alphabet_list[int(s) - 1]
     print('RESULT: ' + result_str)
 
 main()
