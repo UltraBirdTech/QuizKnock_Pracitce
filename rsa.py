77703c2dbd746692016d73f1dd4f2e9bc1dfd6ae
UltraBirdTech <ultrabirdrepti@gmail.com>
2021-11-21 12:00:52 +0900

    add str result

--- rsa.py
+++ rsa.py
@@ -3,6 +3,8 @@
 # 須貝と作れるようになるLIVE「プログラミングを始めよう！」
 # RSA 暗号をプログラムで解いてみる。
 ##################################
+import re
+
 def main():
     p = 37
     q = 71
@@ -10,16 +12,18 @@ def main():
     c = 904 # 暗号文
 
     n = calc_n(p, q)
-    print(n)
+    print("n: " + str(n))
 
     m = calc_m(e, p, q)
-    print(m)
+    print("m: " + str(m))
 
     d = calc_d(m, p, q, e)
-    print(d)
+    print("d: " + str(d))
 
     M = calc_M(c, d, n)
-    print(M)
+    print("M: " + str(M))
+
+    exchange_M_to_str(M)
 
 # p * q を行い n の値を求める
 def calc_n(p, q):
@@ -41,5 +45,39 @@ def calc_d(m, p, q, e):
 # c の d 乗を n で割る
 def calc_M(c, d, n):
     return (c**d) % n
+
+def exchange_M_to_str(m):
+    alphabet_array = {
+        1: "A",
+        2: "B",
+        3: "C",
+        4: "D",
+        5: "E",
+        6: "F",
+        7: "G",
+        8: "H",
+        9: "I",
+        10: "J",
+        11: "K",
+        12: "L",
+        13: "M",
+        14: "N",
+        15: "O",
+        16: "P",
+        17: "Q",
+        18: "R",
+        19: "S",
+        20: "T",
+        21: "U",
+        22: "V",
+        23: "W",
+        24: "X",
+        25: "Y",
+        26: "Z",
+    }
+    result_str = ''
+    for s in  list(filter(None, re.split('(..)', str(m)))):
+        result_str += alphabet_array.get(int(s))
+    print('RESULT: ' + result_str)
 
 main()
